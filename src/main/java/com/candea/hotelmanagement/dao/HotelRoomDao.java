01bd772fdfaf4f0f806869f33d8dfaf3e30e0120
Cândea Marian <candea.dev@gmail.com>
2024-05-17 12:06:04 +0300

    Create HotelRoomDao.java

---
+++ src/main/java/com/candea/hotelmanagement/dao/HotelRoomDao.java
@@ -0,0 +1,9 @@
+package com.candea.hotelmanagement.dao;
+
+import com.candea.hotelmanagement.entity.HotelRoom;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HotelRoomDao extends JpaRepository<HotelRoom, Integer>{
+
+    
+}
